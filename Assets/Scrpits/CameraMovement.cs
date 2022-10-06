d4363c03e82fb90c1cfd079fb7da1db6c4d9a8c6
phatntph11422 <phatntph11422@gmail.com>
2022-10-06 21:34:12 +0700

    B5: Di chuyển cam theo nhân vật và dùng hàm Vector3.Lerp() để tạo sự mượt mà trong di chuyển

---
+++ Assets/Scrpits/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    private void LateUpdate()
+    {
+        //Nếu vị trí cam khác với vị trí target(player)
+        if(transform.position != target.position)
+        {
+            //Tạo ra vector 3 để truyền vào tranform.position.z để cam có thể nhìn thấy nhân vật, nếu để z của nhân vật thì cam sẽ bằng nhân vật sẽ k nhìn thấy gì
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            //Hàm Lerp giúp di chuyển mềm mại hơn
+            //param: 1. Vị trí hiện tại - 2. Vị trí muốn di chuyển đến - 3. làm mịn giữa 1 và 2
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
