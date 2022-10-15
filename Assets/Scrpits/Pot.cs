c46d0750eb8f64aeb10aba067f052110a9f70c01
phatntph11422 <phatntph11422@gmail.com>
2022-10-15 21:24:22 +0700

    B13; Xử lý phá vỡ chậu cây khi thực hiện chém

---
+++ Assets/Scrpits/Pot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    //Khai báo animator
+    private Animator anim;
+    void Start()
+    {
+        //Khởi tạo animator
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+    //Xử lý animation pot vỡ
+    public void Smash()
+    {
+        //set smash = true;
+        anim.SetBool("smash", true);
+        StartCoroutine(breakCo());
+    }
+
+    private IEnumerator breakCo()
+    {
+        //Nghỉ 0.3s
+        yield return new WaitForSeconds(0.3f);
+        this.gameObject.SetActive(false);//Set lại thành không hoạt động
+    }
+}
