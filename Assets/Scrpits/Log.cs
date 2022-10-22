ac26b3ef517d94ef2f347558c313b8a90641550f
phatntph11422 <phatntph11422@gmail.com>
2022-10-22 21:26:58 +0700

    B21: Xử lý amin cho enemy

--- Assets/Scrpits/Log.cs
+++ Assets/Scrpits/Log.cs
@@ -11,8 +11,13 @@ public class Log : Enemy
     public float attackRadius;
     //Vị trí nhà mà log sẽ quay lại
     public Transform homePosition;
+
+    //Khai báo animator
+    private Animator anim;
     void Start()
     {
+        //Khai báo animator
+        anim = GetComponent<Animator>();
         //Setup trạng thái của log là đứng yên
         currentState = EnemyState.idle;
         //Khoi tao rigidbody
@@ -37,8 +42,53 @@ public class Log : Enemy
             {
                 //hàm Vector3.MoveTowards() giúp log di chuyển đến vị trí của nhân vật và cách nhân vật 1 khoảng
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                ChangeAnim(temp - transform.position);
+                //Cập nhật vị trí hiện tại mà log di chuyển đến
                 myRigidbody.MovePosition(temp);
+                
                 ChangeState(EnemyState.walk);
+                //điều khiển anim đứng dậy
+                anim.SetBool("wakeUp", true);
+            }
+        }
+        else if(Vector3.Distance(target.position, transform.position) > chaseRadius) anim.SetBool("wakeUp", false);
+    }
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    //Hàm xử lý thay đổi anim của log
+    private void ChangeAnim(Vector2 direction)
+    {
+        //Kiểm tra xem log di chuyển trái phải hay lên xuống
+        //Vì có cả giá trị âm nên ta phải dùng trị tuyệt đối để so sánh sang trái phải hay lên xuống
+        //Nếu di chuyển sang
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            //Nếu x > 0 => sang phải,  x < 0 => sang trái
+            if (direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if(direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        //Ngược lại di chuyển lên xuống
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            //Nếu y > 0 => lên trên,  y < 0 => xuống dưới
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
             }
         }
     }
